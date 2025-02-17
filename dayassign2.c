d888b9fadd91145185c799cad6e8166a3075f48e
viksha12-rgb <vikshithashetty9@gmail.com>
2025-02-17 22:36:35 +0530

    day1assign2.c

---
+++ dayassign2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() 
+{
+    int N;
+    printf("Enter the number of rows: ");
+    scanf("%d", &N);
+    for (int i = 1; i <= N; i++) 
+    {
+        for (int j = 1; j <= i; j++) 
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+ return 0;
+}
