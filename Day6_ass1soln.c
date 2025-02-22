103efdb41704db91690279d9f4276ee9c78b25ee
vikshitha shetty <vikshithashetty9@gmail.com>
2025-02-22 22:16:07 +0530

    Day6_ass1soln.c

--- Day6_ass1soln.c
+++ Day6_ass1soln.c
@@ -10,12 +10,9 @@ typedef struct {
     int marks;
 } Student;
 
-// Function to compare students for sorting (Descending order of marks)
 int compare(const void *a, const void *b) {
     return ((Student *)b)->marks - ((Student *)a)->marks;
 }
-
-// Binary search function to find student by roll number
 int binary_search(Student students[], int left, int right, int roll_no) {
     while (left <= right) {
         int mid = left + (right - left) / 2;
@@ -28,8 +25,6 @@ int binary_search(Student students[], int left, int right, int roll_no) {
     }
     return -1;
 }
-
-// Function to display students
 void display_students(Student students[], int n) {
     printf("\nStudent Records:\n");
     for (int i = 0; i < n; i++) {
@@ -52,20 +47,14 @@ int main() {
         printf("Enter Roll No, Name, Marks for student %d: ", i + 1);
         scanf("%d %s %d", &students[i].roll_no, students[i].name, &students[i].marks);
     }
-
-    // Sorting students by marks (Descending)
     qsort(students, n, sizeof(Student), compare);
    
     printf("\nSorted Student List (by Marks):\n");
     display_students(students, n);
-   
-    // Display top 3 students
     printf("\nTop 3 Students:\n");
     for (int i = 0; i < (n < 3 ? n : 3); i++) {
         printf("%s (Marks: %d)\n", students[i].name, students[i].marks);
     }
-   
-    // Search for a student by roll number
     int search_roll;
     printf("\nEnter Roll No to search: ");
     scanf("%d", &search_roll);
